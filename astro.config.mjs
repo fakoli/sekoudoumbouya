37516e0b9bbf4ca16c6c441775e9f0dc186bb34c
Sekou Doumbouya <sdoumbouya81@gmail.com>
2025-11-22 21:12:15 -0800

    updated the configuration to handle different base paths dynamically

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+const { PUBLIC_BASE_PATH, SITE } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fakoli.github.io/', // Replace with your actual domain
-  base: '/sekoudoumbouya',
+  site: SITE || 'https://sekoudoumbouya.com',
+  base: PUBLIC_BASE_PATH || '/',
   vite: {
     plugins: [tailwindcss()]
   },
